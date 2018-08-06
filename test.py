a508748f7728b06cfe6e51ae357a33b52bb8e60d
Chad Francis <yamanote1138@gmail.com>
2018-08-05 21:57:15 -0600

    trying different pinouts in test file

--- test.py
+++ test.py
@@ -7,22 +7,27 @@ import time
 # PIN 16 (GPIO23) - EYES OPEN
 # PIN 18 (GPIO24) - EYES CLOSED
 
+MOUTH_OPEN = 17
+MOUTH_CLOSED = 27
+EYES_OPEN = 23
+EYES_CLOSED = 24
+
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(11, GPIO.OUT)
-GPIO.setup(13, GPIO.OUT)
+GPIO.setup(MOUTH_OPEN, GPIO.OUT)
+GPIO.setup(MOUTH_CLOSED, GPIO.OUT)
 
-GPIO.output(11, GPIO.HIGH)
-GPIO.output(13, GPIO.LOW)
-
-time.sleep(3)
-
-GPIO.output(11, GPIO.LOW)
-GPIO.output(13, GPIO.HIGH)
+GPIO.output(MOUTH_OPEN, GPIO.HIGH)
+GPIO.output(MOUTH_CLOSED, GPIO.LOW)
 
 time.sleep(3)
 
-GPIO.output(11, GPIO.LOW)
-GPIO.output(13, GPIO.LOW)
+GPIO.output(MOUTH_OPEN, GPIO.LOW)
+GPIO.output(MOUTH_CLOSED, GPIO.HIGH)
+
+time.sleep(3)
+
+GPIO.output(MOUTH_OPEN, GPIO.LOW)
+GPIO.output(MOUTH_CLOSED, GPIO.LOW)
 
 GPIO.cleanup()
