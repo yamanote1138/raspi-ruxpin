2fa8dfb7400881abf657cc290970e56c9b792e97
Chad Francis <yamanote1138@gmail.com>
2018-08-05 23:20:46 -0600

    changed test file to load pins from config file

--- test.py
+++ test.py
@@ -1,41 +1,49 @@
+import ConfigParser
 import RPi.GPIO as GPIO
 import time
 
+# read config file
+config = ConfigParser.RawConfigParser()
+config.read('main.cfg')
 
-# PIN 11 (GPIO17) - MOUTH OPEN
-# PIN 13 (GPIO27) - MOUTH CLOSED
-# PIN 16 (GPIO23) - EYES OPEN
-# PIN 18 (GPIO24) - EYES CLOSED
+# map configured pins to variables
+MOUTH_OPEN = config.get('pins', 'mouth_open')
+MOUTH_CLOSED = config.get('pins', 'mouth_closed')
+EYES_OPEN = config.get('pins', 'eyes_open')
+EYES_CLOSED = config.get('pins', 'eyes_closed')
 
-MOUTH_OPEN = 11
-MOUTH_CLOSED = 13
-EYES_OPEN = 16
-EYES_CLOSED = 18
-
+# set gpio to map pins using board numbers (as opposed to bcm names)
 GPIO.setmode(GPIO.BOARD)
 
+# designate pins as OUT
 GPIO.setup(MOUTH_OPEN, GPIO.OUT)
 GPIO.setup(MOUTH_CLOSED, GPIO.OUT)
 GPIO.setup(EYES_OPEN, GPIO.OUT)
 GPIO.setup(EYES_CLOSED, GPIO.OUT)
 
+# open the mouth and eyes
 GPIO.output(MOUTH_OPEN, GPIO.HIGH)
 GPIO.output(MOUTH_CLOSED, GPIO.LOW)
 GPIO.output(EYES_OPEN, GPIO.HIGH)
 GPIO.output(EYES_CLOSED, GPIO.LOW)
 
+# wait a second
 time.sleep(1)
 
+# close the mouth and eyes
 GPIO.output(MOUTH_OPEN, GPIO.LOW)
 GPIO.output(MOUTH_CLOSED, GPIO.HIGH)
 GPIO.output(EYES_OPEN, GPIO.LOW)
 GPIO.output(EYES_CLOSED, GPIO.HIGH)
 
+# wait a second
 time.sleep(1)
 
+# set all pins used to off (just to be safe)
 GPIO.output(MOUTH_OPEN, GPIO.LOW)
 GPIO.output(MOUTH_CLOSED, GPIO.LOW)
 GPIO.output(EYES_OPEN, GPIO.LOW)
 GPIO.output(EYES_CLOSED, GPIO.LOW)
 
+# clear pin settings
 GPIO.cleanup()
