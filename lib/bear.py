40456d1e65ad07eec820b52afef0477163a59f9b
Chad Francis <yamanote1138@gmail.com>
2018-10-10 16:24:53 -0600

    prevent servos from opening/closing when already in that state

--- lib/bear.py
+++ lib/bear.py
@@ -7,23 +7,29 @@ class Servo:
     # map configured pins to variables
     self.open_pin = open_pin
     self.close_pin = close_pin
+    self.is_open = None
+
     # designate pins as OUT
     GPIO.setup(self.open_pin, GPIO.OUT)
     GPIO.setup(self.close_pin, GPIO.OUT)
 
   def open(self):
-    GPIO.output( self.open_pin, GPIO.HIGH )
-    GPIO.output( self.close_pin, GPIO.LOW )
-    time.sleep(.5)
-    GPIO.output( self.open_pin, GPIO.LOW )
-    GPIO.output( self.close_pin, GPIO.LOW )
+    if(!self.is_open):
+      GPIO.output( self.open_pin, GPIO.HIGH )
+      GPIO.output( self.close_pin, GPIO.LOW )
+      self.is_open = True
+      time.sleep(.5)
+      GPIO.output( self.open_pin, GPIO.LOW )
+      GPIO.output( self.close_pin, GPIO.LOW )
 
   def close(self):
-    GPIO.output( self.open_pin, GPIO.LOW )
-    GPIO.output( self.close_pin, GPIO.HIGH )
-    time.sleep(.5)
-    GPIO.output( self.open_pin, GPIO.LOW )
-    GPIO.output( self.close_pin, GPIO.LOW )
+    if(!self.is_open):
+      GPIO.output( self.open_pin, GPIO.LOW )
+      GPIO.output( self.close_pin, GPIO.HIGH )
+      self.is_open = False
+      time.sleep(.5)
+      GPIO.output( self.open_pin, GPIO.LOW )
+      GPIO.output( self.close_pin, GPIO.LOW )
 
 class Bear:
   def __init__(self, config):
