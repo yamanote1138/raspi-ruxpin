9de57968d7aa1fab77c46b195e4a8cc33057f441
Chad Francis <yamanote1138@gmail.com>
2018-10-10 16:38:27 -0600

    merging in the bear refactor

--- main.py
+++ main.py
@@ -43,66 +43,66 @@ audio = None
 isRunning = True
 
 def updateMouth():
-    lastMouthEvent = 0
-    lastMouthEventTime = 0
+  lastMouthEvent = 0
+  lastMouthEventTime = 0
 
-    while( audio == None ):
-        time.sleep( 0.1 )
-        
+  while( audio == None ):
+    time.sleep( 0.1 )
+    
     while isRunning:
-        if( audio.mouthValue != lastMouthEvent ):
-            lastMouthEvent = audio.mouthValue
-            lastMouthEventTime = time.time()
+      if( audio.mouthValue != lastMouthEvent ):
+        lastMouthEvent = audio.mouthValue
+        lastMouthEventTime = time.time()
 
-            if( audio.mouthValue == 1 ):
-                GPIO.output( MOUTH_OPEN, GPIO.HIGH )
-                GPIO.output( MOUTH_CLOSED, GPIO.LOW )
-            else:
-                GPIO.output( MOUTH_OPEN, GPIO.LOW )
-                GPIO.output( MOUTH_CLOSED, GPIO.HIGH )
+        if( audio.mouthValue == 1 ):
+          GPIO.output( MOUTH_OPEN, GPIO.HIGH )
+          GPIO.output( MOUTH_CLOSED, GPIO.LOW )
         else:
-            if( time.time() - lastMouthEventTime > 0.4 ):
-                GPIO.output( MOUTH_OPEN, GPIO.LOW )
-                GPIO.output( MOUTH_CLOSED, GPIO.LOW )
+          GPIO.output( MOUTH_OPEN, GPIO.LOW )
+          GPIO.output( MOUTH_CLOSED, GPIO.HIGH )
+        else:
+          if( time.time() - lastMouthEventTime > 0.4 ):
+            GPIO.output( MOUTH_OPEN, GPIO.LOW )
+            GPIO.output( MOUTH_CLOSED, GPIO.LOW )
 
 # A routine for blinking the eyes in a semi-random fashion.
 def updateEyes():
-    while isRunning:
-        GPIO.output( EYES_CLOSED, 1 )
-        GPIO.output( EYES_OPEN, 0 )
-        time.sleep(0.4)
-        GPIO.output( EYES_CLOSED, 0 )
-        GPIO.output( EYES_OPEN, 1 )
-        time.sleep(0.4)
-        GPIO.output( EYES_CLOSED, 1 )
-        GPIO.output( EYES_OPEN, 0 )
-        time.sleep(0.4)
-        GPIO.output( EYES_CLOSED, 0 )
-        GPIO.output( EYES_OPEN, 0 )
-        time.sleep( randint( 5,15) )
-   
-def phrase(myPhrase):
-    audio.play("sounds/"+myPhrase+".wav")
-    return myPhrase
+  while isRunning:
+    GPIO.output( EYES_CLOSED, 1 )
+    GPIO.output( EYES_OPEN, 0 )
+    time.sleep(0.4)
+    GPIO.output( EYES_CLOSED, 0 )
+    GPIO.output( EYES_OPEN, 1 )
+    time.sleep(0.4)
+    GPIO.output( EYES_CLOSED, 1 )
+    GPIO.output( EYES_OPEN, 0 )
+    time.sleep(0.4)
+    GPIO.output( EYES_CLOSED, 0 )
+    GPIO.output( EYES_OPEN, 0 )
+    time.sleep( randint( 5,15) )
+    
+    def phrase(myPhrase):
+      audio.play("sounds/"+myPhrase+".wav")
+      return myPhrase
 
-def talk(myText):
+      def talk(myText):
     os.system( "espeak \",...\" 2>/dev/null" ) # Sometimes the beginning of audio can get cut off. Insert silence.
     time.sleep( 0.5 )
     subprocess.call(["espeak", "-w", "speech.wav", myText, "-s", "130", "-a", "200", "-ven-us+m3","-g","5"])
     audio.play("speech.wav")
     return myText
 
-if(config.getboolean('options', 'move_mouth')):
-    mouthThread = Thread(target=updateMouth)
-    mouthThread.start()
+    if(config.getboolean('options', 'move_mouth')):
+      mouthThread = Thread(target=updateMouth)
+      mouthThread.start()
 
-if(config.getboolean('options', 'move_eyes')):
-    eyesThread = Thread(target=updateEyes)
-    eyesThread.start()     
+      if(config.getboolean('options', 'move_eyes')):
+        eyesThread = Thread(target=updateEyes)
+        eyesThread.start()     
 
-audio = AudioPlayer()
+        audio = AudioPlayer()
 
-web = WebFramework(talk, phrase, phrases)
-isRunning = False
-GPIO.cleanup()
-sys.exit(1)
+        web = WebFramework(talk, phrase, phrases)
+        isRunning = False
+        GPIO.cleanup()
+        sys.exit(1)
