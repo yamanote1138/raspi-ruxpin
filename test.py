a2a4d63de9740c816bbd60b65e355465bfac682f
Chad Francis <yamanote1138@gmail.com>
2018-10-28 21:09:14 -0600

    refactored test file

--- test.py
+++ test.py
@@ -4,10 +4,8 @@ import time
 import RPi.GPIO as GPIO
 
 parser = argparse.ArgumentParser(description='testing open/close of bear motors')
-parser.add_argument("-s", "--servo", choices=["eyes", "mouth"],
-                    help='which servo to operate')
-parser.add_argument("-d", "--direction", choices=["open", "close"],
-                    help='which direction to move')
+parser.add_argument("-e", "--eyes", choices=["open", "close", "o", "c"], help="position of eyes")
+parser.add_argument("-m", "--mouth", choices=["open", "close", "o", "c"], help="position of mouth")
 
 args = parser.parse_args()
 print("setting %s to %s" % (args.servo, args.direction))
@@ -17,21 +15,23 @@ EC_PIN = 24
 MO_PIN = 17
 MC_PIN = 27
 
-if(args.servo == 'eyes'):
-  if(args.direction == 'open'):
-    pin = EO_PIN
-  else:
-    pin = EC_PIN
+if(args.eyes == 'open' or args.eyes == 'o'):
+  epin = EO_PIN
 else:
-  if(args.direction == 'open'):
-    pin = MO_PIN
-  else:
-    pin = MC_PIN
+  epin = EC_PIN
+
+if(args.mouth == 'open' or args.mouth == 'o'):
+  mpin = MO_PIN
+else:
+  mpin = MC_PIN
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(pin, GPIO.OUT)
-GPIO.output(pin, GPIO.HIGH)
+GPIO.setup(epin, GPIO.OUT)
+GPIO.setup(mpin, GPIO.OUT)
+GPIO.output(epin, GPIO.HIGH)
+GPIO.output(mpin, GPIO.HIGH)
 time.sleep(.5)
-GPIO.output(pin, GPIO.LOW)
+GPIO.output(epin, GPIO.LOW)
+GPIO.output(mpin, GPIO.LOW)
 
 GPIO.cleanup()
