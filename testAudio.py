450c52f04e1a8198eaf1bc7bbba495605f3f0049
Chad Francis <yamanote1138@gmail.com>
2020-01-20 00:07:01 -0700

    added exit to audio test

--- testAudio.py
+++ testAudio.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 import sys
-import os
 from lib.audioPlayer import AudioPlayer
 
 # init audio player and play test file
 audio = AudioPlayer()
 filename = "baleeted"
 audio.play("public/sounds/"+filename+".wav")
+sys.exit(1)
