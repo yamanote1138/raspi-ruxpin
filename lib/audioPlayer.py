ddd61d2320a0dde7de2de7652008097ef059a88d
Chad Francis <yamanote1138@gmail.com>
2020-01-26 20:43:13 -0700

    converting audioPlayer to use pyAudio

--- lib/audioPlayer.py
+++ lib/audioPlayer.py
@@ -1,71 +1,60 @@
 #!/usr/bin/env python
-# based on Chippy Ruxpin by Next Thing Co 2015
 
 import alsaaudio as aa
 import audioop
-from time import sleep
-import struct
-import math
-import array
-import numpy as np
+import pyaudio
 import wave
-import os
-import subprocess
 
 class AudioPlayer:
   def __init__(self):
-    subprocess.Popen('amixer cset numid=1 100%' ,shell=True, stdout=subprocess.PIPE ) # Set PA mixer volume to 100%
-    subprocess.Popen('amixer cset numid=2 2' ,shell=True, stdout=subprocess.PIPE ) # Set right mixer to be "right" (2)
-    subprocess.Popen('amixer cset numid=3 1' ,shell=True, stdout=subprocess.PIPE ) # Set left mixer to be "left" (1)
-    subprocess.Popen('amixer cset numid=4 1' ,shell=True, stdout=subprocess.PIPE ) # Set DAC self.output to be "Direct" (2... or 1 for "Mixed" if you prefer)
     self.prevAudiovalue = 0
     self.mouthValue = 0
     self.setVolume(100)
-        
+    self.mixer = aa.Mixer('PCM')
+
   def setVolume(self, volume=75):
-    m = aa.Mixer('PCM')
-    m.setvolume(volume)
-    current_volume = m.getvolume() # Get the current Volume
+    self.mixer.setvolume(volume)
+    current_volume = self.mixer.getvolume() # Get the current Volume
     print("volume set at {}").format(current_volume)
 
   def play(self,fileName):
-    # Initialise matrix
-    matrix=[0,0,0,0,0,0,0,0]
+    # Set chunk size of 1024 samples per data frame
+    chunk = 1024  
 
-    # Set up audio
-    wavfile = wave.open(fileName,'r')
-    chunk = 1024
-    output = aa.PCM(aa.PCM_PLAYBACK, aa.PCM_NORMAL)
-    output.setchannels(1)
-    output.setrate(22050)
-    output.setformat(aa.PCM_FORMAT_S16_LE)
-    output.setperiodsize(chunk)
+    # Create an interface to PortAudio
+    p = pyaudio.PyAudio()
 
-    data = wavfile.readframes(chunk)
-    try:
-      while data!='':
-       output.write(data)
-       # Split channel data and find maximum volume   
-       channel_l=audioop.tomono(data, 2, 1.0, 0.0)
-       channel_r=audioop.tomono(data, 2, 0.0, 1.0)
-       max_vol_factor =5000
-       max_l = audioop.max(channel_l,2)/max_vol_factor
-       max_r = audioop.max(channel_r,2)/max_vol_factor
+    # Open the sound file 
+    wf = wave.open(filename, 'rb')
 
-      for i in range (1,8):
-        self.generateMouthSignal((1<<max_r)-1)
+    # Open a .Stream object to write the WAV file to
+    # 'output = True' indicates that the sound will be played rather than recorded
+    stream = p.open(
+      format = p.get_format_from_width(wf.getsampwidth()),
+      channels = wf.getnchannels(),
+      rate = wf.getframerate(),
+      output = True
+    )
 
-        data = wavfile.readframes(chunk)
-    except:
-      data = None
+    # Read data in chunks
+    data = wf.readframes(chunk)
 
-      os.system( '/etc/init.d/alsa-utils restart' )
-      sleep( .25 )
+    # Play the sound by writing the audio data to the stream
+    while data != '':
+      stream.write(data)
+      data = wf.readframes(chunk)
+      rms = audioop.rms(data, 2)
+      generateMouthSignal(rms)
+
+    # Close and terminate the stream
+    stream.close()
+    p.terminate()
 
   def generateMouthSignal(self,val):
-    delta = val - self.prevAudiovalue 
-    if( delta < -2 or val == 0 ):
-      self.mouthValue = 0
-    elif( delta > 0 ):
-      self.mouthValue = 1
-      self.prevAudiovalue = val
+    print(val)
+    # delta = val - self.prevAudiovalue 
+    # if( delta < -2 or val == 0 ):
+    #   self.mouthValue = 0
+    # elif( delta > 0 ):
+    #   self.mouthValue = 1
+    #   self.prevAudiovalue = val
--- testAudio.py
+++ testAudio.py
@@ -3,11 +3,11 @@ import sys
 from lib.audioPlayer import AudioPlayer
 
 audio = AudioPlayer()
-filename = "baleeted"
 
 audio.setVolume(100)
 
-# print("trying to play test file")
-# audio.play("public/sounds/"+filename+".wav")
+print("trying to play test file")
+filename = "baleeted"
+audio.play("public/sounds/"+filename+".wav")
 
 sys.exit(1)
