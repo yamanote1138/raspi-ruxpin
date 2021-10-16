03670d84969e08de90ee9465e0a02ec0cf85430e
Chad Francis <yamanote1138@gmail.com>
2021-10-15 22:29:03 -0600

    adding args to test

--- test.py
+++ test.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python
+import sys
+import getopt
 import RPi.GPIO as GPIO
 import time
+
+def main(argv):
+  motor = ''
+  direction = ''
+  speed = 0
+
+  try:
+    opts, args = getopt.getopt(argv,"hm:d:s:")
+  except getopt.GetoptError:
+    print('test.py -m motor -d direction -s speed')
+    sys.exit(2)
+  for opt, arg in opts:
+      if opt == '-h':
+        print('test.py -d direction -s speed')
+        sys.exit()
+      elif opt=="-m":
+        motor = arg
+      elif opt =="-d":
+        direction = arg
+      elif opt=="-s":
+        speed = int(arg)
+  
+  print(format('motor: {} direction: {} speed: {}', motor, direction, speed))
 
 # set config variables
 pwm_freq = 2000
@@ -10,27 +35,30 @@ cdir_pin = 23
 speed = 100
 duration = .5
 
-# use Broadcom pin designations
-GPIO.setmode(GPIO.BCM)
+# # use Broadcom pin designations
+# GPIO.setmode(GPIO.BCM)
 
-# designate pins as OUT
-GPIO.setup(pwm_pin, GPIO.OUT)
-GPIO.setup(dir_pin, GPIO.OUT)
-GPIO.setup(cdir_pin, GPIO.OUT)
+# # designate pins as OUT
+# GPIO.setup(pwm_pin, GPIO.OUT)
+# GPIO.setup(dir_pin, GPIO.OUT)
+# GPIO.setup(cdir_pin, GPIO.OUT)
 
-# initialize PWM
-pwm = GPIO.PWM(pwm_pin, pwm_freq)
+# # initialize PWM
+# pwm = GPIO.PWM(pwm_pin, pwm_freq)
 
-# set pin levels
-GPIO.output(dir_pin, GPIO.HIGH)
-GPIO.output(cdir_pin, GPIO.LOW)
+# # set pin levels
+# GPIO.output(dir_pin, GPIO.HIGH)
+# GPIO.output(cdir_pin, GPIO.LOW)
 
-pwm.start(speed)
-time.sleep(duration)
-pwm.stop()
+# pwm.start(speed)
+# time.sleep(duration)
+# pwm.stop()
 
-# unset pin levels
-GPIO.output(dir_pin, GPIO.LOW)
-GPIO.output(cdir_pin, GPIO.LOW)
+# # unset pin levels
+# GPIO.output(dir_pin, GPIO.LOW)
+# GPIO.output(cdir_pin, GPIO.LOW)
 
-GPIO.cleanup()
+# GPIO.cleanup()
+
+if __name__ == "__main__":
+  main(sys.argv[1:])
