45ae29ca6ed83d003f0d6c94e10f4c7c994e5489
Chad Francis <yamanote1138@gmail.com>
2021-10-15 20:32:37 -0600

    setting ip address manually

--- lib/webFramework.py
+++ lib/webFramework.py
@@ -7,7 +7,7 @@ import socket
 class WebFramework:
   def __init__(self,bear):
     # self.ip = [(s.connect(('8.8.8.8', 80)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]
-    self.ip = '127.0.0.1'
+    self.ip = '192.168.1.66'
     self.e = 'o'
     self.m = 'c'
 
